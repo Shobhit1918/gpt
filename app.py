79e4a0836246d8f1991b73cd47d1b275b7ea52fb
Shobhit Gosain <gosainshobhit@gmail.com>
2023-09-29 09:36:13 +0530

    Initial commit

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
 from htmlTemplates import css, bot_template, user_template
 from langchain.llms import HuggingFaceHub
+from langchain.callbacks import get_openai_callback
 
 def get_pdf_text(pdf_docs):
     text = ""
@@ -17,6 +18,7 @@ def get_pdf_text(pdf_docs):
         for page in pdf_reader.pages:
             text += page.extract_text()
     return text
+#should return a single list of text with all the contents.1)We initialized a variable called text.2) for each pdf uploaded in pdf_docs.3)loop through all of the pages of each of the pdf
 
 
 def get_text_chunks(text):
@@ -32,14 +34,14 @@ def get_text_chunks(text):
 
 def get_vectorstore(text_chunks):
     embeddings = OpenAIEmbeddings()
-    # embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
+    #embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
     vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
     return vectorstore
 
 
 def get_conversation_chain(vectorstore):
     llm = ChatOpenAI()
-    # llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512})
+    #llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512})
 
     memory = ConversationBufferMemory(
         memory_key='chat_history', return_messages=True)
@@ -52,22 +54,22 @@ def get_conversation_chain(vectorstore):
 
 
 def handle_userinput(user_question):
-    response = st.session_state.conversation({'question': user_question})
-    st.session_state.chat_history = response['chat_history']
+    with get_openai_callback() as cb:
+        response = st.session_state.conversation({'question': user_question})
+        st.session_state.chat_history = response['chat_history']
 
-    for i, message in enumerate(st.session_state.chat_history):
-        if i % 2 == 0:
-            st.write(user_template.replace(
-                "{{MSG}}", message.content), unsafe_allow_html=True)
-        else:
-            st.write(bot_template.replace(
-                "{{MSG}}", message.content), unsafe_allow_html=True)
-
+        for i, message in enumerate(st.session_state.chat_history):
+            if i % 2 == 0:
+                st.write(user_template.replace(
+                    "{{MSG}}", message.content), unsafe_allow_html=True)
+            else:
+                st.write(bot_template.replace(
+                    "{{MSG}}", message.content), unsafe_allow_html=True)
+    print(cb)        
 
 def main():
     load_dotenv()
-    st.set_page_config(page_title="Chat with multiple PDFs",
-                       page_icon=":books:")
+    st.set_page_config(page_title="EkoBeta1")
     st.write(css, unsafe_allow_html=True)
 
     if "conversation" not in st.session_state:
@@ -75,29 +77,47 @@ def main():
     if "chat_history" not in st.session_state:
         st.session_state.chat_history = None
 
-    st.header("Chat with multiple PDFs :books:")
+    st.header("Enter your query")
     user_question = st.text_input("Ask a question about your documents:")
     if user_question:
         handle_userinput(user_question)
 
-    with st.sidebar:
-        st.subheader("Your documents")
-        pdf_docs = st.file_uploader(
-            "Upload your PDFs here and click on 'Process'", accept_multiple_files=True)
-        if st.button("Process"):
-            with st.spinner("Processing"):
-                # get pdf text
-                raw_text = get_pdf_text(pdf_docs)
+    # with st.sidebar:
+    #     st.subheader("Your documents")
+    #     pdf_docs = st.file_uploader(
+    #         "Upload your PDFs here and click on 'Process'", accept_multiple_files=True)
+    #     if st.button("Process"):
+    #         with st.spinner("Processing"):
+    #             # get pdf text
+    #             raw_text = get_pdf_text(pdf_docs)
 
-                # get the text chunks
-                text_chunks = get_text_chunks(raw_text)
+    #             # get the text chunks
+    #             text_chunks = get_text_chunks(raw_text)
 
-                # create vector store
-                vectorstore = get_vectorstore(text_chunks)
+    #             # create vector store
+    #             vectorstore = get_vectorstore(text_chunks)
 
-                # create conversation chain
-                st.session_state.conversation = get_conversation_chain(
-                    vectorstore)
+    #             # create conversation chain
+    #             st.session_state.conversation = get_conversation_chain(
+    #                 vectorstore)
+    # Define the hardcoded file paths to your PDFs
+    pdf_docs = [
+        "Queries.pdf","FAQ(eps+conn).pdf"
+        
+    ]
+
+    with st.spinner("Processing"):
+        # Get pdf text
+        raw_text = get_pdf_text(pdf_docs)
+
+        # Get the text chunks
+        text_chunks = get_text_chunks(raw_text)
+
+        # Create vector store
+        vectorstore = get_vectorstore(text_chunks)
+
+        # Create conversation chain
+        st.session_state.conversation = get_conversation_chain(vectorstore)
 
 
 if __name__ == '__main__':
